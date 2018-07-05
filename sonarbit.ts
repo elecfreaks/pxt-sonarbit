b8e343477aaecf5cac50d2fc75cc3a93badf9b3d
ElecFreaks <support@elecfreaks.com>
2018-07-05 15:40:21 +0800

    Update and rename robit.ts to sonarbit.ts

---
+++ sonarbit.ts
@@ -0,0 +1,34 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace sonarbit {
+
+    /**
+ * get Ultrasonic
+ * @param jpin, eg: 3
+*/
+    //% blockId=robit_ultrasonic block="Ultrasonic|pin %pin"
+    //% weight=10
+    export function sonarbit(pin: DigitalPin): number {
+
+        // send pulse
+        pins.setPull(pin, PinPullMode.PullNone);
+        pins.digitalWritePin(pin, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(pin, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(pin, 0);
+
+        // read pulse
+        let d = pins.pulseIn(pin, PulseValue.High, 23000);  // 8 / 340 = 
+        return d * 5 / 3 / 58;
+    }
+
+}
