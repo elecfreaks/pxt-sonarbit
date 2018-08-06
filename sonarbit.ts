7922ee1330614e6815f1f10eb0c98334289fe144
ElecFreaks <support@elecfreaks.com>
2018-08-06 12:16:55 +0800

    Update sonarbit.ts

--- sonarbit.ts
+++ sonarbit.ts
@@ -4,18 +4,30 @@
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
+enum Distance_Unit {
+    //% block="mm" enumval=0
+    Distance_Unit_mm,
+
+    //% block="cm" enumval=1
+    Distance_Unit_cm,
+
+    //% block="inch" enumval=2
+    Distance_Unit_inch,
+}
+
+
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon="\uf013"
+//% weight=10 color=#0fbc11 icon="\uf140"
 namespace sonarbit {
 
     /**
- * get Ultrasonic
-*/
-    //% blockId=sonarbit block="Ultrasonic distance at|pin %pin"
+    * get Ultrasonic distance
+    */
+    //% blockId=sonarbit block="Ultrasonic distance in unit %distance_unit |at|pin %pin"
     //% weight=10
-    export function sonarbit_distance(pin: DigitalPin): number {
+    export function sonarbit_distance(distance_unit: Distance_Unit, pin: DigitalPin): number {
 
         // send pulse
         pins.setPull(pin, PinPullMode.PullNone);
@@ -27,7 +39,24 @@ namespace sonarbit {
 
         // read pulse
         let d = pins.pulseIn(pin, PulseValue.High, 23000);  // 8 / 340 = 
-        return d * 5 / 3 / 58;
+        let distance = d * 10 * 5 / 3 / 58;
+
+
+        switch (distance_unit) {
+            case 0:
+                return distance //mm
+                break;
+            case 1:
+                return distance / 10  //cm
+                break;
+            case 2:
+                return distance / 25  //inch
+                break;
+            default:
+                return 0
+
+        }
+
     }
 
 }
